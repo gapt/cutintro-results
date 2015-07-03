10ad874fe04a28de684b5cdc8e21a307a70b67c9
Gabriel Ebner <gebner@gebner.org>
2015-07-03 13:08:02 +0200

    Add script for automated analysis of experiment results.

---
+++ do_analysis.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -d "$1" ]; then
+  echo "Usage: ./do_analysis.sh YYYY-MM-DD"
+  exit 1
+fi
+
+set -x
+cd "$1"
+
+sed '
+  s@filename = \\".*\\"@filename = \\"CutIntroDataLog.txt\\"@;
+  s@previous_filename = \\".*\\"@previous_filename = \\"../2015-03-15/CutIntroDataLog.txt\\"@;
+' ../cutintro.ipynb >cutintro.ipynb
+ipython3 nbconvert --inplace --execute --to notebook cutintro.ipynb
+
+python3 ../summarize.py CutIntroDataLog.txt
+python3 ../diffgrammarsizes.py CutIntroDataLog.txt >diffgrammars.txt
