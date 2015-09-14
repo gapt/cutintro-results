9d807e552af5ea49f867527d66be2ae0616c185d
Gabriel Ebner <gebner@gebner.org>
2015-09-14 10:49:50 +0200

    IPython packages got split up.

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "cutintro-experiments";
   buildInputs = with python34Packages; [
-    ipython
+    ipython nbconvert notebook
     pandas matplotlib
     sbt openjdk8
     prover9 veriT
